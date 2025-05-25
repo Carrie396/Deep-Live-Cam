2f0f1bf4ecd66de30fdc8b21027330e10cdc4fee
Carrie396 <carriegreen396@gmail.com>
2025-05-25 19:28:41 +0000

    Update face_enhancer.py - TO ADD LINK

--- modules/processors/frame/face_enhancer.py
+++ modules/processors/frame/face_enhancer.py
@@ -33,7 +33,7 @@ def pre_check() -> bool:
     conditional_download(
         download_directory_path,
         [
-            "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth"
+            ""
         ],
     )
     return True
